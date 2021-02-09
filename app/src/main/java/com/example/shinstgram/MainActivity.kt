71efc9cee8c5a2bd534afce89567f2904c69bf32
stevey-sy <sinsy89@hanmail.net>
2021-02-09 15:37:34 +0900

    [FIX] Main 화면 / Bottom Navigation Bar 구현 완료

--- app/src/main/java/com/example/shinstgram/MainActivity.kt
+++ app/src/main/java/com/example/shinstgram/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.shinstgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.shinstgram.navigation.AlarmFragment
+import com.example.shinstgram.navigation.DetailViewFragment
+import com.example.shinstgram.navigation.GridFragment
+import com.example.shinstgram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_home -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+
+                return true
+            }
+            R.id.action_photo -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+            var userFragment = UserFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+            return true
+            }
+            R.id.action_account -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
+
 }
+
---
+++ app/src/main/java/com/example/shinstgram/navigation/GridFragment.kt
@@ -0,0 +1,21 @@
+package com.example.shinstgram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shinstgram.R
+
+class GridFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // view 세팅
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container, false)
+        return view
+    }
+}
