e5b70192b6e752ab6c81637f9e984de95382b200
stevey-sy <sinsy89@hanmail.net>
2021-02-10 18:32:02 +0900

    [FIX] 좋아요 버튼 클릭 이벤트 추가. 기능 작동 성공

--- app/src/main/java/com/example/shinstgram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/shinstgram/navigation/DetailViewFragment.kt
@@ -11,17 +11,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.shinstgram.R
 import com.example.shinstgram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
     var firestore: FirebaseFirestore? = null
+    var uid : String? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // view 세팅
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
 
         view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
@@ -31,6 +34,7 @@ class DetailViewFragment : Fragment() {
     // 외부 클래스를 항상 참조하는 inner class
     inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+        // uid 리스트
         var contentUidList: ArrayList<String> = arrayListOf()
 
         init {
@@ -76,18 +80,56 @@ class DetailViewFragment : Fragment() {
             viewholder.detailviewitem_explain_textview.text = contentDTOs!![position].explain
 
             // likes
-            viewholder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![0].favoriteCount
+            viewholder.detailviewitem_favoritecounter_textview.text =
+                "Likes " + contentDTOs!![0].favoriteCount
 
             //profile image
 //            Glide.with(holder.itemView.context)
 //                .load(contentDTOs!![position].imageUrl)
 //                .into(viewholder.detailviewitem_profile_image)
 
+            // 좋아요 버튼 클릭 이벤트
+            viewholder.detailviewitem_favorite_imageview.setOnClickListener {
+                favoriteEvent(position)
+            }
+            // 좋아요 count 와 빈 하트가 색칠 되도록 처리
+            // 내 uid 가 좋아요 누른 사람 목록에 포함되어 있다면
+            if(contentDTOs!![position].favorites.containsKey(uid)) {
+                // 좋아요 버튼 클릭한 경우
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
 
+            } else {
+                // 좋아요 버튼 클릭하지 않은 경우
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
         }
 
         override fun getItemCount(): Int {
             return contentDTOs.size
+        }
+
+        fun favoriteEvent(position : Int) {
+            //
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction {transaction ->
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                // 좋아요가 이미 클릭되어 있는 경우, 아닌 경우
+                if(contentDTO!!.favorites.containsKey(uid)){
+                    // 좋아요 개수 변경
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount -1
+                    // 좋아요 누른사람 정보에서 현재 사용자의 uid 를 제거
+                    contentDTO?.favorites.remove(uid)
+                } else {
+                    // 눌려있지 않다
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount +1
+                    contentDTO?.favorites[uid!!] = true
+                }
+                // 수정된 좋아요 정보를 업로드 한다.
+                transaction.set(tsDoc,contentDTO)
+
+            }
         }
 
 
