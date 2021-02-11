5016393b4380d6df6ba625782515228e4e581c8f
stevey-sy <sinsy89@hanmail.net>
2021-02-11 19:13:17 +0900

    [FIX] Grid Fragment / 게시글 GridLayout 으로 표시되도록 수정

--- app/src/main/java/com/example/shinstgram/navigation/GridFragment.kt
+++ app/src/main/java/com/example/shinstgram/navigation/GridFragment.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.shinstgram.R
+import com.example.shinstgram.navigation.model.ContentDTO
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
 class GridFragment : Fragment() {
+    var firestore : FirebaseFirestore? = null
+    var fragmentView : View? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,7 +26,48 @@ class GridFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // view 세팅
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container, false)
-        return view
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container, false)
+        firestore = FirebaseFirestore.getInstance()
+        fragmentView?.gridfragment_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView?.gridfragment_recyclerview?.layoutManager = GridLayoutManager(activity, 3)
+        return fragmentView
+    }
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        init {
+            firestore?.collection ("images")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                // 스냅샷이 null 이면 종료
+                if(querySnapshot == null) return@addSnapshotListener
+
+                // Get data
+                for (snapshot in querySnapshot.documents) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val width = resources.displayMetrics.widthPixels /3
+
+            val imageview = ImageView(parent.context);
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView) : RecyclerView.ViewHolder(imageview) {
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl)
+                .apply(RequestOptions().centerCrop()).into(imageview)
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
     }
 }
