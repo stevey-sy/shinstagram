233c589ac52e78777f4b88f55bb9703436bb7e1f
stevey-sy <sinsy89@hanmail.net>
2021-02-12 15:20:53 +0900

    [FIX] AlarmDTO / kind 변수 부연설명 주석 추가

--- app/src/main/java/com/example/shinstgram/navigation/model/AlarmDTO.kt
+++ app/src/main/java/com/example/shinstgram/navigation/model/AlarmDTO.kt
@@ -4,6 +4,10 @@ data class AlarmDTO (
     var destinationUid : String? = null,
     var userId : String? = null,
     var uid : String? = null,
+    // kind = 알람 구분 flag 값
+    // like = 0
+    // comment = 1
+    // follow = 2
     var kind : Int? = null,
     var message : String? = null,
     var timestamp : Long? = null
