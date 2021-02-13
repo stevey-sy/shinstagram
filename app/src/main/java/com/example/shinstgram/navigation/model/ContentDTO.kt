963af2626d71e7ca7cce7c05b32692ffa7f46d6f
stevey-sy <sinsy89@hanmail.net>
2021-02-13 16:36:44 +0900

    [FIX] Content DTO / 댓글 개수를 저장할 변수 추가

--- app/src/main/java/com/example/shinstgram/navigation/model/ContentDTO.kt
+++ app/src/main/java/com/example/shinstgram/navigation/model/ContentDTO.kt
@@ -8,6 +8,7 @@ data class ContentDTO (
                    var userId : String? = null,
                    var timestamp : Long? = null,
                    var favoriteCount : Int = 0,
+                   var commentCount : Int = 0,
                    var favorites : MutableMap<String,Boolean> = HashMap()) {
     data class Comment(var uid : String? = null,
                        var userId : String? = null,
