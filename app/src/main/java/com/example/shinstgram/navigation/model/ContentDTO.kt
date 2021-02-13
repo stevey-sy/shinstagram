4bd6625f70bffc31b972ae495e62e935053fe3bc
stevey-sy <sinsy89@hanmail.net>
2021-02-13 20:54:14 +0900

    [FIX] content DTO / 등록 시간정보 담을 변수 추가

--- app/src/main/java/com/example/shinstgram/navigation/model/ContentDTO.kt
+++ app/src/main/java/com/example/shinstgram/navigation/model/ContentDTO.kt
@@ -9,6 +9,7 @@ data class ContentDTO (
                    var timestamp : Long? = null,
                    var favoriteCount : Int = 0,
                    var commentCount : Int = 0,
+                   var uploadTime : String? = null,
                    var favorites : MutableMap<String,Boolean> = HashMap()) {
     data class Comment(var uid : String? = null,
                        var userId : String? = null,
