c40f233c39a9b28889c1881bdc35f4d5258bad11
stevey-sy <sinsy89@hanmail.net>
2021-02-15 11:56:39 +0900

    [FIX] AlarmFragment.kt / 알람 메시지에 시간 표시되도록 수정

--- app/src/main/java/com/example/shinstgram/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/shinstgram/navigation/AlarmFragment.kt
@@ -1,6 +1,8 @@
 package com.example.shinstgram.navigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +17,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_alarm.view.*
 import kotlinx.android.synthetic.main.item_comment.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AlarmFragment : Fragment() {
 
@@ -39,13 +44,15 @@ class AlarmFragment : Fragment() {
        init {
             val uid = FirebaseAuth.getInstance().currentUser?.uid
             // 내 아이디와 작성자의 아이디가 같을 때
-            FirebaseFirestore.getInstance().collection("alarms").whereEqualTo("destinationUid", uid)
+            FirebaseFirestore.getInstance().collection("alarms")
+                .whereEqualTo("destinationUid", uid)
+//                .orderBy("timestamp")
                 .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                     alarmDTOList.clear()
                     if(querySnapshot == null) return@addSnapshotListener
                     // snapshot 에 담긴 데이터를 하나씩 나열한다
                     for (snapshot in querySnapshot.documents) {
-                        alarmDTOList.add(snapshot.toObject(AlarmDTO::class.java)!!)
+                        alarmDTOList.add(0, snapshot.toObject(AlarmDTO::class.java)!!)
                     }
                     notifyDataSetChanged()
                 }
@@ -54,7 +61,7 @@ class AlarmFragment : Fragment() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
             // 아이템 디자인 정의
             var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
-            view.commentviewitem_textview_regtime.visibility = View.GONE
+//            view.commentviewitem_textview_regtime.visibility = View.GONE
             // custom view holder 에서 view 를 가공한다.
             return CustomViewHolder(view)
         }
@@ -75,6 +82,12 @@ class AlarmFragment : Fragment() {
                         .into(view.commentviewitem_imageview_profile)
                 }
            }
+
+            val format= SimpleDateFormat("yyyyMMddHHmmss")
+            val formatTime : String? = format.format(alarmDTOList[position].timestamp)
+            val convertedDate : String? = TimeConverter.CreateDataWithCheck(formatTime)
+            view.commentviewitem_textview_regtime.text = convertedDate
+
 
             when (alarmDTOList[position].kind) {
                 // 좋아요 알람 경우
@@ -99,6 +112,17 @@ class AlarmFragment : Fragment() {
 
         override fun getItemCount(): Int {
             return alarmDTOList.size
+        }
+
+        @SuppressLint("SimpleDateFormat")
+        private fun getDateTime(longDate: Long?): String {
+            try {
+                val sdf = SimpleDateFormat("yyyy/MM/dd")
+                val netDate = longDate?.times(1000)?.let { Date(it) }
+                return sdf.format(netDate)
+            } catch (e: Exception) {
+                return e.toString()
+            }
         }
 
     }
