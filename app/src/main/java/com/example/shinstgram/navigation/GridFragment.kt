9a1596f5990564872a050d77cd2753de64c12d4d
stevey-sy <sinsy89@hanmail.net>
2021-02-16 15:33:38 +0900

    [FIX] GirdFragment.kt / 이미지 클릭하면 게시글로 이동하하는 이벤트 추가

--- app/src/main/java/com/example/shinstgram/navigation/GridFragment.kt
+++ app/src/main/java/com/example/shinstgram/navigation/GridFragment.kt
@@ -1,6 +1,8 @@
 package com.example.shinstgram.navigation
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -34,14 +36,20 @@ class GridFragment : Fragment() {
     }
     inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        // uid 리스트
+        var contentUidList: ArrayList<String> = arrayListOf()
         init {
-            firestore?.collection ("images")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            firestore?.collection ("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                contentDTOs.clear()
+                contentUidList.clear()
                 // 스냅샷이 null 이면 종료
                 if(querySnapshot == null) return@addSnapshotListener
-
                 // Get data
                 for (snapshot in querySnapshot.documents) {
-                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                    contentDTOs.add(0, snapshot.toObject(ContentDTO::class.java)!!)
+                    contentUidList.add(0, snapshot.id)
+                    contentUidList.add(0, snapshot.id)
+                    Log.d("Grid Fragment / 콘텐트 UID", snapshot.id)
                 }
                 notifyDataSetChanged()
             }
@@ -63,6 +71,18 @@ class GridFragment : Fragment() {
             val imageview = (holder as CustomViewHolder).imageview
             Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl)
                 .apply(RequestOptions().centerCrop()).into(imageview)
+
+            // 이미지 클릭하면 article 로 이동
+            imageview.setOnClickListener { v ->
+                // 필요한 것 = current user's uid
+                //          =  content uid
+                val intent = Intent(v.context, CommentActivity::class.java)
+                // 게시글 번호
+                intent.putExtra("contentUid", contentUidList[position])
+                // 게시글 작성자 uid
+                intent.putExtra("destinationUid", contentDTOs[position].uid)
+                startActivity(intent)
+            }
         }
 
         override fun getItemCount(): Int {
